deb9c8f5316a3e4ea26bf352dafb5ffa02e7683c
gamertop13lol <36103611+gamertop13lol@users.noreply.github.com>
2021-04-10 10:44:58 +0100

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %UserProfile%
+python "%UserProfile%/a'mu_v1-0.py"
+pause
---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+
+copy ".\why not.png" "%UserProfile%"
+copy ".\calculadora.py" "%UserProfile%"
+copy ".\a'mu_v1-0.py" "%UserProfile%"
+pause
